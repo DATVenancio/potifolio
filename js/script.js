97e5be16e7c5f60cf62e4af0ab4a2ea104db6273
Daniel Venâncio <danielvenancioif@gmail.com>
2021-03-05 00:49:44 -0300

    mudança entre curriculo e contato

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function botaoCurriculo(){
+    document.getElementById("curriculo-texto").style.display="block";
+    document.getElementById("contato-texto").style.display="none";
+}
+function botaoContato(){
+    document.getElementById("curriculo-texto").style.display="none";
+    document.getElementById("contato-texto").style.display="block";
+}
