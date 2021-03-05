7bcd0d1ad132fadb363b7d4a84b75d8a451ad8ec
Daniel Venâncio <danielvenancioif@gmail.com>
2021-03-05 13:54:27 -0300

    funcionalidades portifolio prontas

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ function botaoContato(){
     document.getElementById("curriculo-texto").style.display="none";
     document.getElementById("contato-texto").style.display="block";
 }
+
+function botaoJogos(){
+    document.getElementById("div-jogos").style.display="block";
+    document.getElementById("div-web").style.display="none";
+    document.getElementById("div-desktop").style.display="none";
+}
+function botaoWeb(){
+    document.getElementById("div-jogos").style.display="none";
+    document.getElementById("div-web").style.display="block";
+    document.getElementById("div-desktop").style.display="none";
+}
+function botaoDesktop(){
+    document.getElementById("div-jogos").style.display="none";
+    document.getElementById("div-web").style.display="none";
+    document.getElementById("div-desktop").style.display="block";
+}
